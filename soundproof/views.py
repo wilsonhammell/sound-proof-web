ac4a660fde3144c18b88dbe3a79a44823187f256
hammell <17wsh@caslab.queensu.ca>
2022-03-06 00:46:20 -0500

    recording

--- soundproof/views.py
+++ soundproof/views.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
+from datetime import datetime
+import os
 
 views = Blueprint('views', __name__)
 
-@views.route("/")
+@views.route("/", methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    if request.method == 'POST':
+        audio=request.files['file']
+        file=datetime.now().strftime("%d%m%Y%H%M%S")
+        with open(f'soundproof/audio/recordings/{file}' + '.wav', 'wb') as destination:
+            audio.save(destination)
+        return render_template('home.html', request="POST", user=current_user)
+
+    else:
+        return render_template('home.html', user=current_user)
