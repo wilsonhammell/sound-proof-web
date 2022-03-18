3fdd73dc71e379524260a6334d9e1c676d030a40
hammell <17wsh@caslab.queensu.ca>
2022-03-18 11:20:49 -0400

    audio encryption and time offset

--- soundproof/views.py
+++ soundproof/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
 from datetime import datetime
+import time
 import os
 
 views = Blueprint('views', __name__)
@@ -11,9 +12,9 @@ def home():
     if request.method == 'POST':
         audio=request.files['file']
         file=datetime.now().strftime("%d%m%Y%H%M%S")
-        with open(f'soundproof/audio/recordings/{file}' + '.wav', 'wb') as destination:
+        with open(f'soundproof/audio/recordings/{file}.wav', 'wb') as destination:
             audio.save(destination)
-        return render_template('home.html', request="POST", user=current_user)
+        return render_template('home.html', user=current_user)
 
     else:
-        return render_template('home.html', user=current_user)
+        return render_template('home.html', user=current_user, servertime=time.time())
