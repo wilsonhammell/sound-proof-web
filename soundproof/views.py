5ac9cedc98dbc92f3d56c35608b9c0450510bbbf
hammell <17wsh@caslab.queensu.ca>
2022-04-08 03:49:07 -0400

    trying something

--- soundproof/views.py
+++ soundproof/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, request, redirect
+from flask import Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
 from datetime import datetime
 
@@ -8,10 +8,3 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
      return render_template('home.html', user=current_user)
-
-@views.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
